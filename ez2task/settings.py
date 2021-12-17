fd5f0314c6adab1eb1e3cc80f97cd32e22f6d2cd
igorsimb <fatalhope@gmail.com>
2021-12-17 17:38:25 +0300

    Settings

--- ez2task/settings.py
+++ ez2task/settings.py
@@ -14,17 +14,20 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 from dotenv import load_dotenv
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
+DEBUG = os.getenv("DEBUG") == 'True'
 
 DEPLOYED = False
 
@@ -35,7 +38,7 @@ DEPLOYED = False
 # else:
 #     DEBUG = True
 
-DEBUG = os.getenv('DEBUG')
+
 
 ALLOWED_HOSTS = ['localhost', 'ez2task.herokuapp.com', 'ez2task.com']
 
